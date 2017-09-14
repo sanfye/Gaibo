0ace6effb9df8453486cfd27e5bd6d0971008c4b
sanfye <sanfye1018@163.com>
2017-09-15 00:17:04 +0800

    添加打包成war的依赖和方法

--- gaibo-parent/gaibo-query-server/src/main/java/com/gaibo/biz/GaiboQueryServerApplication.java
+++ gaibo-parent/gaibo-query-server/src/main/java/com/gaibo/biz/GaiboQueryServerApplication.java
@@ -2,9 +2,22 @@ package com.gaibo.biz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GaiboQueryServerApplication {
+public class GaiboQueryServerApplication /*extends SpringBootServletInitializer*/{
+	
+	/**
+	 * 需要在打包成war包运行时，需要放开此注释
+	 * @param args
+	 * @author sangwenhao
+	 * @date 2017年9月15日
+	 */
+	/*@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(GaiboQueryServerApplication.class);
+	}*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(GaiboQueryServerApplication.class, args);
